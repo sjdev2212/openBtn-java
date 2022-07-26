7f5a3b67663c7a5b826b4aa81b167ae7ea1c05db
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-07-26 20:01:32 -0300

    agrega function precio con iva

--- src/main/java/com/example1/Data.java
+++ src/main/java/com/example1/Data.java
@@ -10,6 +10,8 @@ public class Data {
         long largo = 1500;
         boolean boleano = true;
 
+
+
         System.out.println(numero);
         System.out.println(decimalD);
         System.out.println(decimalf);
@@ -17,8 +19,11 @@ public class Data {
         System.out.println(letra);
         System.out.println(largo);
         System.out.println(boleano);
+       double resultado = precioConIva(20);
+        System.out.println(resultado);
 
-
-
+    }
+    static double precioConIva (double precio) {
+        return (precio *  21 / 100)  + precio;
     }
 }
