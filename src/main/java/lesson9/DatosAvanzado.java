485614a50af1257285ebf7a9fad311707f11421f
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-08-03 00:23:56 -0300

    agrega funcion recorrer array unidmensional

--- src/main/java/lesson9/DatosAvanzado.java
+++ src/main/java/lesson9/DatosAvanzado.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class DatosAvanzado {
     public static void main(String[] args) {
         System.out.println( reverse("lesson9"));
+        int[] intArray = new int[]{ 1,2,3,4,5,6,7,8,9,10 };
+        recorrerArr(intArray);
 
 
     }
@@ -21,4 +23,11 @@ public class DatosAvanzado {
         }
        return Arrays.toString(b).join("",b);
     }
+
+public static void recorrerArr (int[] arr){
+
+    for (int j : arr) {
+        System.out.println(j);
+    }
+}
 }
